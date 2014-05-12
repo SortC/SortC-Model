463c2b3776e74a9668ae9739ff5ae9323f111ad9
stsander <Sander_Stefanie@gmx.net>
2014-05-12 10:42:31 +0200

    lauffähig
    die nicht benötigten Klassen mit rechtsklick eigenschaften -> vom build
    ausschließen -> ja

--- SortCModel/Model/Model.cpp
+++ SortCModel/Model/Model.cpp
@@ -2,22 +2,22 @@
 //
 
 #include "stdafx.h"
-//#include "BubbleSort.h"
+#include "BubbleSort.h"
 #include "Step.h"
 #include <iostream> 
 
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
-	vector<int> values;
+	vector<int> values(2,5);
 	vector<int>* v1 = &values;
 	values.push_back(1);
 	values.push_back(4);
 	cout << values[2];
+
 	Step *s1 = new Step(v1, Operation::COMP ,1);
-	cin ;
 	//BubbleSort *bs = new BubbleSort(values);
+	getchar();
 	return 0;	
 }
 
