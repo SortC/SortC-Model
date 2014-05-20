8abdf97f0c9569a938fb7b1b0a3f342bd3d6d074
Toaster93 <webersteven93@gmail.com>
2014-05-20 22:19:33 +0200

    Testfunktion

--- SortCModel/Model/Model.cpp
+++ SortCModel/Model/Model.cpp
@@ -5,47 +5,72 @@
 #include "BubbleSort.h"
 #include "Step.h"
 #include <iostream>
-
 #include <ctime>
+#include <typeinfo>
+#include <iomanip>
+#include <random>
+
+
+#define ANZWERTE 5
+#define LINEWIDTH 40
+const char FILLCHAR = '-';
 
 using namespace std;
-int _tmain(int argc, _TCHAR* argv[])
-{
-    int zahlen [5];
-    for(int i = 0; i < 5; i++){
-        zahlen[i] = 5-i;
-    }
-    BubbleSort bs(zahlen,5);
 
-    clock_t begin = clock();
-	
-    bs.sort();
-	
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout <<"Passed time: "<< elapsed_secs << " seconds" << endl;
-	
-    
-    bs.ausgabe();
+void testAlgorithm(Algorithm* algo, bool isBackwardSorted) {
+	cout << setw(LINEWIDTH) << setfill(FILLCHAR) << "" <<  endl;
+	cout << typeid(*algo).name() << endl << endl;
+	cout << "Ausgangstupel: " ;
 
-	//Step testStep(1,5, Operation::COMP, 5);
+	int zahlen [ANZWERTE];
+	if (isBackwardSorted)
+	{
+		for(int i = 0; i < ANZWERTE; i++){
+			zahlen[i] = ANZWERTE-i;
+			cout << " | "<< zahlen[i] ;
+		}
+	}
+	else
+	{
+		srand(time(nullptr));
+		for (int i = 0; i < ANZWERTE; i++)
+		{
+			zahlen[i] = rand() % 100;
+			cout << " | "<< zahlen[i] ;
+		}
+	}
+	cout << " |" << endl;
 
-	//cout << testStep.toString();
-	
-	Step* currStep = bs.getNextStep(); 
-	int i = 1;
+
+	/* clock_t begin = clock(); */
+	algo->sort();
+
+	/*clock_t end = clock();
+
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC / 1000;
+	cout <<"Passed time: "<< elapsed_secs << " milliseconds" << endl<< endl;*/
+
+	Step* currStep = algo->getNextStep(); 
+	cout << "No.\t Explanation" << endl; 
 	do{
 		if(currStep->getOperation() == Operation::SWAP){
-			cout <<currStep->getNumber() << ".Schritt: --> " << currStep->toString();
-			cout << endl;
-			i++;
-		}
-		currStep = bs.getNextStep();
-	}while(currStep != NULL);
-	
-	getchar();
-	
 
+			cout << "[" <<currStep->getNumber() << "] \t " << currStep->toString();
+		}
+		currStep = algo->getNextStep();
+	}while(currStep != NULL);
+	cout << setw(LINEWIDTH) << setfill(FILLCHAR) << "" <<  endl << endl<< endl;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Testreihe mit " << ANZWERTE << " Werten" << endl;
+	cout << setw(LINEWIDTH) << setfill(FILLCHAR) << endl;
+	testAlgorithm(new BubbleSort(ANZWERTE),true);
+	testAlgorithm(new BubbleSort(ANZWERTE),false);
+
+	getchar();
 	return 0;
 }
+
 
